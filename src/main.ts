9bf2bc23a39b9b3492fb67efd272d9697ce9e0c3
He110te4m <he110te4m@gmail.com>
2021-05-21 15:57:50 +0800

    (update): update code struct

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from 'home/mod_launch/App.vue'
 import router from 'router/index';
 import store from 'store/index';
+
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
+
 import 'common/style/index.less';
 
 const app = createApp(App);
