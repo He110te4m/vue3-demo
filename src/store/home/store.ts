8c5119a87907d4507c56c5bc745845f176c37ce3
He110te4m <he110te4m@gmail.com>
2021-05-24 10:35:31 +0800

    (add): add vuex util type.

--- src/store/home/store.ts
+++ src/store/home/store.ts
@@ -6,7 +6,9 @@
  * @Last Modified time: 2021-05-23 14:12:56
  */
 
-import { CommitOptions, DispatchOptions, Store } from 'vuex';
+import type { Store } from 'vuex';
+import type { FixCommit, FixDispatch, FixGetter } from '../types';
+
 import { homeActions, HomeActions } from './actions';
 import { homeGetters, HomeGetters } from './getters';
 import { HomeMutations, homeMutations } from './mutations';
@@ -19,14 +21,8 @@ export const homeStore = {
     actions: homeActions
 };
 
-export type HomeStore = Omit<Store<HomeState>, 'commit' | 'getters'> & {
-    getters: {
-        [K in keyof HomeGetters]: ReturnType<HomeGetters[K]>;
-    };
-    dispatch<K extends keyof HomeActions>(key: K, payload: Parameters<HomeActions[K]>[1], options?: DispatchOptions): ReturnType<HomeActions[K]>;
-    commit<K extends keyof HomeMutations, P extends Parameters<HomeMutations[K]>[1]>(
-        key: K,
-        payload: P,
-        options?: CommitOptions
-    ): ReturnType<HomeMutations[K]>;
+export type HomeStore = Omit<Store<HomeState>, 'commit' | 'getters' | 'dispatch'> & {
+    getters: FixGetter<HomeGetters>;
+    dispatch: FixDispatch<HomeActions>;
+    commit: FixCommit<HomeMutations>;
 };
