dcb7d8d77b795d166a1f49d53b5d766ede30f5a0
He110te4m <i@he110.info>
2021-06-06 13:22:46 +0800

    (add): add search field.

--- src/home/mod_home/layout/search.vue
+++ src/home/mod_home/layout/search.vue
@@ -1,5 +1,16 @@
 <template>
-    <div></div>
+    <div class="search">
+        <i :class="['search__icon', 'link', 'iconfont', isShowSearchInput ? 'icon-close' : 'icon-search']"
+           @click="reverseSearchShowStatus()"></i>
+        <teleport to="body">
+            <div class="search-field"
+                 v-show="isShowSearchInput">
+                <a-input-search class="search-field__input"
+                                size="large"
+                                @search="onSearch()" />
+            </div>
+        </teleport>
+    </div>
 </template>
 
 <script lang="ts">
@@ -10,15 +21,63 @@
  * @description:
  */
 
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
     name: 'Search',
     setup: () => {
-        return {};
+        const keyword = ref('');
+        const isShowSearchInput = ref(false);
+
+        return {
+            keyword,
+            isShowSearchInput,
+            reverseSearchShowStatus () {
+                isShowSearchInput.value = !isShowSearchInput.value;
+            },
+            onSearch () {}
+        };
     }
 });
 </script>
 
 <style lang="less">
+.search {
+    position: fixed;
+    top: 1rem;
+    right: 2rem;
+    z-index: var(--search-button-index);
+
+    &__icon {
+        float: right;
+        font-size: 2.8rem;
+    }
+
+    &__input {
+        float: right;
+        width: 20rem;
+        margin: .6rem;
+    }
+}
+
+.search-field {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: var(--full-width);
+    height: var(--full-height);
+
+    background-color: var(--white);
+    z-index: var(--search-index);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    backdrop-filter: blur(3rem);
+
+    &__input {
+        width: 90%;
+        max-width: 80rem;
+    }
+}
 </style>
